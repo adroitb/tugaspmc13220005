2ccf6e44630d7c7548543da6b6ab30aa0158fa3b
AdroThePVP <13220005@mahasiswa.itb.ac.id>
2022-01-30 10:37:30 +0700

    Update - Membaca tipe file dengan melihat file signature

--- Tugas 3/bacafile.c
+++ Tugas 3/bacafile.c
@@ -1,18 +1,47 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main() {
+    printf("Masukkan nama file: ");
+    char array[100];
+    scanf("%s", array);
+    FILE *fileptr;
+    char *bufferpdf;
+    char *bufferjpg;
+    long filelen;
+    int i;
+    int pdf, jpg;
 
-char FILE_NAME[] = "bacatext.txt";
-FILE *myfile;
-
-int main(){
-    myfile = fopen(FILE_NAME, "r");
-    const char *get_filename_ext(const char *FILE_NAME) {
-        const char *dot = strrchr(FILE_NAME, '.');
-        if(!dot || dot == FILE_NAME) return "";
-        return dot + 1;
+    fileptr = fopen(array, "rb");
+    fseek(fileptr, 0, SEEK_END);
+    filelen = ftell(fileptr);
+    rewind(fileptr);
+    bufferpdf = (char *)malloc((filelen+1)*sizeof(char));
+    bufferjpg = (char *)malloc((filelen+1)*sizeof(char));
+    for(i = 0; i < 5; i++) { //Mengambil Hex PDF
+       fread(bufferpdf+i, 1, 1, fileptr);
+    }
+    for(i =-1 ; i < 4; i++) { //Mengambil Hex JPG
+       fread(bufferjpg+i, 1, 1, fileptr);
     }
 
-    printf("Tipe File : %s\n", get_filename_ext(FILE_NAME));
+    printf("File len: %ld\n", filelen);
+    printf("%s\n",*&bufferpdf);
+    printf("%s\n",*&bufferjpg);
+    pdf = strcmp(bufferpdf, "%PDF-"); //Membandingkan code PDF
+    jpg = strcmp(bufferjpg, "JFIF"); //Membandingkan code JPG
+    if (pdf == 0){
+        printf("\nFile yang dipilih adalah file PDF");
+    }
+    else if (jpg == 0){
+        printf("\nFile yang dipilih adalah file JPG");
+    }
+    else{
+        printf("\nFile yang dipilih bukan file PDF maupun JPG.");
+    }
+    fclose(fileptr); // Close the file
+
+    return 0;
 }
+
