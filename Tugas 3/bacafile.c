90dd23d043f409de2dce852663170bb82047f827
AdroThePVP <13220005@mahasiswa.itb.ac.id>
2022-01-31 20:36:15 +0700

    Update on else if

--- Tugas 3/bacafile.c
+++ Tugas 3/bacafile.c
@@ -15,24 +15,26 @@ int main() {
     fread(head, sizeof(head), 1, dat);
 
     unsigned char signaturepng[8] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
-    unsigned char signaturepdf[8] = {0x25, 0x50, 0x44, 0x46, 0x2D, 0x31, 0x2E, 0x37};
+    unsigned char signaturepdf[5] = {0x25, 0x50, 0x44, 0x46, 0x2D};
     unsigned char signaturejpg[8] = {0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46};
 
     if (!memcmp(signaturepng, head, 8))
     {
         printf("\n%s adalah file PNG", array);
-    };
-    if (!memcmp(signaturepdf, head, 8)){
-        printf("\n%s adalah file PDF", array);
+
     }
-    if (!memcmp(signaturejpg, head, 8)){
+    else if (!memcmp(signaturepdf, head, 5)){
+        printf("\n%s adalah file PDF", array);
+        return 0;
+    }
+    else if (!memcmp(signaturejpg, head, 8)){
         printf("\n%s adalah file JPG", array);
+        return 0;
     }
     else{
         printf("\n%s tidak diketahui jenis filenya", array);
     }
 
     fclose(dat);
-    return 0;
 }
 
